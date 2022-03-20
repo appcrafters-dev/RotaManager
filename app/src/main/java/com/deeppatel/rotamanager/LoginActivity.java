a7236cd502915f851bed58c30bfcca33edb7179e
pruthvi145 <pruthvipatel145@gmail.com>
2022-03-21 00:53:35 +0530

    feat: login with firebase

---
+++ app/src/main/java/com/deeppatel/rotamanager/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.deeppatel.rotamanager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+    private final String TAG = "LoginActivity";
+    Toast errorToast;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button loginBtn = findViewById(R.id.activity_login_btn_login);
+        EditText emailEt = findViewById(R.id.activity_login_et_email);
+        EditText inviteCodeEt = findViewById(R.id.activity_login_et_invite_code);
+        loginBtn.setOnClickListener(view -> performLogin(emailEt.getText().toString(), inviteCodeEt.getText().toString()));
+    }
+
+    private void performLogin(String email, String inviteCode) {
+        String errorMessage = validateForm(email, inviteCode);
+        if (!errorMessage.isEmpty()) {
+            showErrorMessage(errorMessage);
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, inviteCode)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                          TODO: redirect
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Exception exception = task.getException();
+                            if (exception.getClass() == FirebaseAuthInvalidUserException.class) {
+                                showErrorMessage("User not found");
+                            } else if (exception.getClass() == FirebaseAuthInvalidCredentialsException.class) {
+                                showErrorMessage("Invalid credentials");
+                            } else {
+                                showErrorMessage("Authentication failed");
+                                Log.w(TAG, "signInWithEmail:failure", exception);
+                            }
+                        }
+                    }
+                });
+    }
+
+    private String validateForm(String email, String inviteCode) {
+        if (email.isEmpty()) return "Email is required";
+        if (!isValidEmail(email)) return "Invalid email";
+        if (inviteCode.isEmpty()) return "InviteCode is required";
+        return "";
+    }
+
+    private Boolean isValidEmail(String value) {
+        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(value);
+        return matcher.find();
+    }
+
+    private void showErrorMessage(String message) {
+        if (message == null || message.isEmpty()) return;
+        if (errorToast != null) errorToast.cancel();
+        errorToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        errorToast.show();
+    }
+
+}
--- app/src/main/java/com/deeppatel/rotamanager/MainActivity.java
+++ app/src/main/java/com/deeppatel/rotamanager/MainActivity.java
@@ -1,8 +1,11 @@
 package com.deeppatel.rotamanager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+            startActivity(intent);
+            finish();
+        }, 3000);
 
     }
 }
