990bb399c7a836caea1e917304f968e182a38d6a
nimit2801 <mnimitsavant@outlook.com>
2022-03-28 11:36:35 +0530

    make request

--- app/src/main/java/com/deeppatel/rotamanager/member/RequestTimeChangeRequest.java
+++ app/src/main/java/com/deeppatel/rotamanager/member/RequestTimeChangeRequest.java
@@ -1,13 +1,19 @@
 package com.deeppatel.rotamanager.member;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.deeppatel.rotamanager.R;
 import com.deeppatel.rotamanager.helpers.Navigate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RequestTimeChangeRequest extends AppCompatActivity {
     private ImageView back;
@@ -15,7 +21,7 @@ public class RequestTimeChangeRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_time_change_request);
-
+        makeRequest("2AMnInXgtgMeqWKiTTuf1spgkw13", "VrTucto4h5sauZH1JxZy");
         back = findViewById(R.id.backButtonToolbar);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +29,21 @@ public class RequestTimeChangeRequest extends AppCompatActivity {
                 Navigate.replace(RequestTimeChangeRequest.this,MemberHomePage.class);
             }
         });
+    }
+
+    public void makeRequest(String uid, String schedid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users").document(uid).collection("Schedule").document(schedid).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            DocumentSnapshot doc = task.getResult();
+                            Log.i("doc", doc.getData().toString());
+                        }
+                        else
+                            Log.i("doc", "Doc not found");
+                    }
+                });
     }
 }
